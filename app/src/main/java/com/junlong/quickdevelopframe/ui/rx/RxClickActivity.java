6edcdf49a6970317d4399477abaf53ed273a9e26
巴黎没有摩天轮 <1056407060@qq.com>
2017-07-17 13:29:35 +0800

    添加缓存工具类

--- app/src/main/java/com/junlong/quickdevelopframe/ui/rx/RxClickActivity.java
+++ app/src/main/java/com/junlong/quickdevelopframe/ui/rx/RxClickActivity.java
@@ -8,6 +8,7 @@ import com.junlong.framecorelibrary.BaseApplication;
 import com.junlong.framecorelibrary.base.BaseMvcActivity;
 import com.junlong.framecorelibrary.rx.rxbase.OnClickCallBack;
 import com.junlong.framecorelibrary.rx.rxbase.RxClick;
+import com.junlong.framecorelibrary.util.CacheUtils;
 import com.junlong.quickdevelopframe.R;
 
 public class RxClickActivity extends BaseMvcActivity {
@@ -22,6 +23,8 @@ public class RxClickActivity extends BaseMvcActivity {
     protected void initView() {
         Button btJoggle = bindView(R.id.bt_joggle);
         final TextView tvCount = bindView(R.id.tv_count);
+        long size = CacheUtils.getInstance().getCacheSize();
+        tvCount.setText(String.valueOf(size));
         RxClick.rxOnClick(btJoggle, 2, new OnClickCallBack() {
             @Override
             public void OnClick() {
