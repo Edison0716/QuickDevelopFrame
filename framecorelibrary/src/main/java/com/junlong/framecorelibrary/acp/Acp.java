a4e5f98baf54525d77802bec784207a9df540108
巴黎没有摩天轮 <1056407060@qq.com>
2017-07-26 11:07:25 +0800

    6.0+ 动态权限封装

--- framecorelibrary/src/main/java/com/junlong/framecorelibrary/acp/Acp.java
+++ framecorelibrary/src/main/java/com/junlong/framecorelibrary/acp/Acp.java
@@ -5,6 +5,33 @@ import android.content.Context;
 /**
  * Created by hupei on 2016/4/26.
  */
+/*
+*使用方法
+*Acp.getInstance(this).request(new AcpOptions.Builder()
+        .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ,Manifest.permission.READ_PHONE_STATE
+        ,Manifest.permission.SEND_SMS)
+        //以下为自定义提示语、按钮文字
+        .setDeniedMessage()
+        .setDeniedCloseBtn()
+        .setDeniedSettingBtn()
+        .setRationalMessage()
+        .setRationalBtn()
+        .build(),
+        new AcpListener(){
+
+@Override
+public void onGranted(){
+        writeSD();
+        getIMEI();
+        }
+
+@Override
+public void onDenied(List<String> permissions){
+        makeText(permissions.toString()+"权限拒绝");
+        }
+        });
+*/
 public class Acp {
 
     private static Acp mInstance;
