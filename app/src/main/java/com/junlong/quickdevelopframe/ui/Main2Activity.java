af49ba82bd19e01e94bdda76034c39967e51d9b7
巴黎没有摩天轮 <1056407060@qq.com>
2017-09-30 10:33:16 +0800

    将EasyPermission 更改为 RxPermisson

--- app/src/main/java/com/junlong/quickdevelopframe/ui/Main2Activity.java
+++ app/src/main/java/com/junlong/quickdevelopframe/ui/Main2Activity.java
@@ -1,34 +1,25 @@
 package com.junlong.quickdevelopframe.ui;
 
 import android.Manifest;
-import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.junlong.framecorelibrary.base.BaseMvcActivity;
-import com.junlong.framecorelibrary.easypermissions.AfterPermissionGranted;
-import com.junlong.framecorelibrary.easypermissions.AppSettingsDialog;
-import com.junlong.framecorelibrary.easypermissions.EasyPermissions;
 import com.junlong.framecorelibrary.glide.GlideUtil;
 import com.junlong.framecorelibrary.glide.RequestCallBack;
+import com.junlong.framecorelibrary.permissions.RxPermissions;
 import com.junlong.framecorelibrary.rx.rxtools.OnEventCallBack;
 import com.junlong.framecorelibrary.rx.rxtools.RxDoubleClick;
 import com.junlong.quickdevelopframe.R;
 
-import java.util.List;
+import io.reactivex.functions.Consumer;
 
-public class Main2Activity extends BaseMvcActivity implements View.OnClickListener, EasyPermissions.PermissionCallbacks {
+public class Main2Activity extends BaseMvcActivity implements View.OnClickListener {
 
-    private static final String TAG = "MainActivity";
-    private static final String[] LOCATION_AND_CONTACTS =
-            {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.READ_CONTACTS};
 
-    private static final int RC_CAMERA_PERM = 123;
-    private static final int RC_LOCATION_CONTACTS_PERM = 124;
-
-    private List<String> mValueList;
+    private RxPermissions mPermissions;
 
     @Override
     protected int setScreenOrientation() {
@@ -46,7 +37,6 @@ public class Main2Activity extends BaseMvcActivity implements View.OnClickListen
             }
         });
         ImageView ivImage = bindView(R.id.iv_image);
-        //GlideUtil.loadBlurImage(this, R.mipmap.ic_launcher, ivImage, 18);
         GlideUtil.loadImage(this, R.mipmap.avatar, ivImage, new RequestCallBack() {
             @Override
             public void requestSuccess() {
@@ -60,6 +50,7 @@ public class Main2Activity extends BaseMvcActivity implements View.OnClickListen
         });
 
         bindView(R.id.bt_permissions).setOnClickListener(this);
+        mPermissions = new RxPermissions(this);
     }
 
     @Override
@@ -91,51 +82,19 @@ public class Main2Activity extends BaseMvcActivity implements View.OnClickListen
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.bt_permissions:
-                cameraTask();
+                mPermissions.request(Manifest.permission.CAMERA)
+                                    .subscribe(new Consumer<Boolean>() {
+                                @Override
+                                public void accept(Boolean aBoolean) throws Exception {
+                                    if (aBoolean) {
+                                        showInfoToast("权限已申请");
+                                    } else {
+                                        showAlertToast("权限已拒绝");
+                                    }
+                                }
+                        });
                 break;
         }
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        // Forward results to EasyPermissions
-        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
-    }
-
-    private boolean hasCameraPermission() {
-        return EasyPermissions.hasPermissions(this, Manifest.permission.CAMERA);
-    }
-
-    @AfterPermissionGranted(RC_CAMERA_PERM)
-    public void cameraTask() {
-        if (hasCameraPermission()) {
-            // Have permission, do the thing!
-            Toast.makeText(this, "TODO: Camera things", Toast.LENGTH_LONG).show();
-        } else {
-            // Ask for one permission
-            EasyPermissions.requestPermissions(
-                    this,
-                    getString(R.string.rationale_camera),
-                    RC_CAMERA_PERM,
-                    Manifest.permission.CAMERA);
-        }
-
-    }
-
-
-    @Override
-    public void onPermissionsGranted(int requestCode, List<String> perms) {
-        showSuccessToast(getString(R.string.grant_success));
-    }
-
-    @Override
-    public void onPermissionsDenied(int requestCode, List<String> perms) {
-        // (Optional) Check whether the user denied any permissions and checked "NEVER ASK AGAIN."
-        // This will display a dialog directing them to enable the permission in app settings.
-        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
-            new AppSettingsDialog.Builder(this).build().show();
-        }
-    }
 }
